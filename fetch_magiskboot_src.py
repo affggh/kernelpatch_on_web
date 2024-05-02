3e03895f66003990fb4a2bb25a902111c4449c99
affggh <879632264@qq.com>
2024-05-02 12:39:28 +0800

    add fetch magiskboot latest source script

---
+++ fetch_magiskboot_src.py
@@ -0,0 +1,16 @@
+from urllib import request
+import json
+import os
+
+API = "https://api.github.com/repos/ookiineko/magiskboot_build/releases/latest"
+
+# fetch magiskboot source
+resp = request.urlopen(API)
+
+jsonData = resp.read()
+
+release = json.loads(jsonData)
+
+for asset in release['assets']:
+    if asset['name'].endswith('src.tar.xz'):
+        os.system(f"wget {asset['browser_download_url']}")
